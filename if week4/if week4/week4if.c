0e78435942eeff4965f72e865b324167423d4457
pluto135 <126849412+pluto135@users.noreply.github.com>
2023-03-24 14:19:43 +0900

    homework

---
+++ if week4/if week4/week4if.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+main(void) {
+	int score = 75;
+
+	if (score > 80) {
+		printf("당신의 점수는 %d점 입니다.\n", score);
+		printf("당신의 학점은 A+입니다.\n");
+	}
+	else if (score > 70) {
+		printf("당신의 학점은 A입니다.");
+	}
+	else if (score > 60) {
+		printf("당신의 학점은 B+입니다.");
+	}
+}
